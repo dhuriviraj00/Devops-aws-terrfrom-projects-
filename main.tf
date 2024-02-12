85decb5aef663c0f26eece0098823cb9ca173512
dhuriviraj00 <144763674+dhuriviraj00@users.noreply.github.com>
2024-02-12 23:23:47 +0530

    add file projects

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "ec2" {
+  ami           = "${var.ami_id}"
+  instance_type = "${var.instance_type}"
+
+  tags = {
+    Name = "HelloWorldterraformcloud"
+    Team= "devops"
+  }
+}
+
